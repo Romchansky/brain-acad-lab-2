12afbce6c1782e1f22a2688fc746ee16bc60c3d4
romka <romkapetrenko8@gmail.com>
2020-03-26 20:05:29 +0200

    Changes

--- Lab4/src/ua/zp/braincad/LessonFour.java
+++ Lab4/src/ua/zp/braincad/LessonFour.java
@@ -1,34 +1,63 @@
 package ua.zp.braincad;
 
+
 import static java.lang.System.*;
 
 public class LessonFour {
     public static void main(String[] args) {
 
-        cycleFor();
+        learnDebug();
         logicalExpressions();
+        numbers();
+        daysOfWeek();
+        numbersFactorial();
 
     }
 
-    public static void cycleFor() {
+    private static void learnDebug() {
         for (int i = 0; i < 10; i++) {
             out.println(i);
         }
     }
 
-    public static void logicalExpressions() {
+    private static void logicalExpressions() {
         boolean a = true && false || true && !false; // false || true => true
         out.println(a);
         boolean b = false || false && !true && false; //  false && false => false
         out.println(b);
         boolean c = !false && (false || true && true); // !false && true => true
         out.println(c);
-        boolean d = 4>8 && !(7==0); // false && false => false
+        boolean d = 4 > 8 && !(7 == 0); // false && false => false
         out.println(d);
-        boolean e = !(true && false || false && true) && !(true && 4<=5); //  true && false => false
+        boolean e = !(true && false || false && true) && !(true && 4 <= 5); //  true && false => false
         out.println(e);
-
 
     }
 
+    private static void numbers() {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                out.println(i + " -> Четное");
+            } else {
+                out.println(i + " -> Нечетное");
+            }
+        }
+    }
+
+    private static void daysOfWeek() {
+        String daysArray[] = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+        for (int i = 0; i < daysArray.length; i++) {
+            if (i == 5 || i == 6) {
+                out.println(daysArray[i] + " -> " + "Выходной день");
+            } else {
+                out.println(daysArray[i] + " -> " + "Рабочий день");
+            }
+        }
+    }
+
+    private static void numbersFactorial(){
+
+    }
 }
+
+
